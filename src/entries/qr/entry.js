f343b1eb4e102fb3bb5c6af3174e874e2d443d53
jhon sebastian valencia <sebasjsv97@gmail.com>
2020-03-23 16:37:38 -0500

    se elimina topnotifications del punto de entrada del qr

--- src/entries/qr/entry.js
+++ src/entries/qr/entry.js
@@ -7,7 +7,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "GlobalAssets/theme/pages/css/pages.min.css";
 import "GlobalAssets/theme/pages/js/pages.min.js";
 import "GlobalAssets/theme/assets/plugins/font-awesome/css/font-awesome.min.css";
-import "GlobalAssets/theme/assets/js/cerok_libraries/notifications/topNotification.js";
 
 Vue.config.productionTip = false;
 
